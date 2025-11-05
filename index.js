49defcf1dc6dc29efa3287838291f499dfae9b91
hafiz89ali <hafiz89.ali@gmail.com>
2025-11-06 06:11:56 +0800

    rewrite index

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-console.log("System is running.");
-
 import express from "express";
-import database from "./database/connection.js";
 import router from "./routes/routes.js";
 
 const app = express();
 const PORT = 3000;
 
-// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// routes
+// router
 app.use(router);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}.`);
+  console.log(`System is running on port ${PORT}.`);
 });
